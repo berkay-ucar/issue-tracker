0c0be83ac135adf89e43ef6599dd4a46eb3b3f38
Berkay Ucar <berkayucar80@gmail.com>
2024-12-30 12:57:40 +0300

    Handling errors 1

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -19,8 +19,12 @@ const NewIssuePage = () => {
     <form
       className="max-w-xl space-y-3"
       onSubmit={handleSubmit(async (data) => {
-        await axios.post("/api/issues", data);
-        router.push("/issues");
+        try {
+          await axios.post("/api/issues", data);
+          router.push("/issues");
+        } catch (error) {
+          console.log(error);
+        }
       })}
     >
       <TextField.Root
