7120af280e69d39ab963183493f787fb4a8ee617
Berkay Ucar <berkayucar80@gmail.com>
2025-01-25 14:17:03 +0300

    fix error

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -16,10 +16,9 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
       await axios.delete("/api/issues/" + issueId);
       router.push("/issues/list");
       router.refresh();
-    } catch (error) {
+    } catch {
       setDeleting(false);
       setError(true);
-      console.log(error);
     }
   };
 
